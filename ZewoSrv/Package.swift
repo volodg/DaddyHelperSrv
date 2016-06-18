7c5e2dae13e880b4c5b165bf4bbe89acf8acd5d9
vlg <gorbenko.vova@gmail.com>
2016-06-18 18:29:49 +0300

    add Posgree

--- ZewoSrv/Package.swift
+++ ZewoSrv/Package.swift
@@ -5,5 +5,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/VeniceX/HTTPServer.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/Zewo/Router.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/PostgreSQL.git", majorVersion: 0, minor: 8)
         ]
 )
