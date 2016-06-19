21ab750768a785946a9d17057a861d526a74bf4f
vlg <gorbenko.vova@gmail.com>
2016-06-19 17:46:58 +0300

    parse log json

--- ZewoSrv/Sources/dbTools.swift
+++ ZewoSrv/Sources/dbTools.swift
@@ -10,12 +10,26 @@ import PostgreSQL
 
 func getOpenConnection() throws -> Connection {
 
+    #if os(Linux)
+        let host = "ec2-174-129-223-35.compute-1.amazonaws.com"
+        let port = 5432
+        let database = "db046pq77t4051"
+        let username = "foxbaunewkojkx"
+        let password = "MzH90RnReUbhnjqHmdOcbyCmuZ"
+    #else
+        let host = "localhost"
+        let port = 5432
+        let database = "logger_development"
+        let username = "volodg"
+        let password = "H4d3yl8x"
+    #endif
+
     let config = Connection.ConnectionInfo(
-        host: "localhost",
-        port: 5432,
-        databaseName: "logger_development",
-        username: "volodg",
-        password: "H4d3yl8x",
+        host: host,
+        port: port,
+        databaseName: database,
+        username: username,
+        password: password,
         options: nil,
         tty: nil)
 
